b86457dc859f408c115b4d260f4295dc6f68cd86
thongbh <huuthong207@gmail.com>
2018-05-15 17:09:20 +0700

    comit Login using Spring MVC ver1.0

---
+++ demoLogin/src/jbr/springmvc/service/UserService.java
@@ -0,0 +1,18 @@
+package jbr.springmvc.service;
+
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+
+public class UserService {
+
+	public void register(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public User validateUser(Login login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
